f7845ef0d3513820b9cfc4ded991f60aa942a03f
Adrian Fernando <Samiadrian55@gmail.com>
2026-04-10 12:41:12 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,11 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttpImport from "pino-http";
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
-
-const pinoHttp =
-  (pinoHttpImport as any).default || pinoHttpImport;
 
 const app: Express = express();
 
